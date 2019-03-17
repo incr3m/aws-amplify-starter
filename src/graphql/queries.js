b5140950fd3a8dbbf54ab60e9a7c1ba3f5f34579
jim <thecastlefaller@gmail.com>
2019-03-17 19:11:08 +0800

    Custom datasource

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -5,3 +5,26 @@ export const appVersion = `query AppVersion {
   appVersion
 }
 `;
+export const getTodo = `query GetTodo($id: ID!) {
+  getTodo(id: $id) {
+    id
+    name
+    description
+  }
+}
+`;
+export const listTodos = `query ListTodos(
+  $filter: ModelTodoFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      name
+      description
+    }
+    nextToken
+  }
+}
+`;
